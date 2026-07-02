cd67fa242bb0a1099556cc0efff6e2e96d1bf0bf
preethik2008 <preethik.25it@kongu.edu>
2026-07-02 17:38:06 +0530

    Completed Task 24 - Product Card Design

---
+++ 24-product-card/js/app.js
@@ -0,0 +1,19 @@
+document.querySelectorAll(".cart").forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+alert("Product added to cart!");
+
+});
+
+});
+
+document.querySelectorAll(".buy").forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+alert("Proceeding to checkout...");
+
+});
+
+});
